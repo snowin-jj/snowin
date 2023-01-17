6998a7ad12425e4b5100773e80598aed1770863a
devsnowin <snowin.developer@gmail.com>
2023-01-17 16:30:31 +0530

    navbar updates

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,15 @@ module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {
+      keyframes: {
+        'open-menu': {
+          '0%': { transform: 'translateY(-100%)' },
+          '100%': { transform: 'translateY(0%)' },
+        },
+      },
+      animation: {
+        'open-menu': 'open-menu 0.5s ease-in-out forwards',
+      },
       screens: {
         xl: '1380px',
       },
